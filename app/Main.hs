01b60a341b7fd295338fd91ef58187b0e757ccc4
H.G. Heady <headyhud@gmail.com>
2020-09-08 01:02:14 -0700

    correct for first compound query of nonrepeating variables

--- app/Main.hs
+++ app/Main.hs
@@ -11,14 +11,13 @@ import Data.Text (Text, splitOn, pack)
 import qualified Data.Text as T
 import Lib
 
-main :: IO ()
+main :: IO [()]
 main = do
   ingested <- getContents
   let input = lines ingested
-  let input' =  map fromJust $ filter isJust $ map (parse . scrub') input
+  let input' =  map fromJust $ filter isJust $ map (parse . scrub) input
   -- mapM (putStrLn . show) input'
   factEngine Map.empty input'
-  fePrint "Fin"
 
 factEngine :: (Map Text Node) -> [Input] -> IO [()]
 factEngine m (i:is) = do
@@ -26,39 +25,55 @@ factEngine m (i:is) = do
     factEngine m' is
   where
     engine :: (Map Text Node) -> Maybe Node -> Input -> IO (Map Text Node)
-    engine m _ f@(Fact  _ _) = addFact  m f
+    engine m _  f@(Fact  _ _) = addFact  m f
     engine m mn q@(Query _ _) = runQuery m mn q
-factEngine _ [] = do return [()]
+factEngine _ [] = return [()]
 
 addFact :: (Map Text Node) -> Input -> IO (Map Text Node)
-addFact m f = do
-  return $ addNode m f
+addFact m f = do return $ addNode m f
 
 runQuery :: (Map Text Node) -> Maybe Node -> Input -> IO (Map Text Node)
-runQuery m mn (Query p els) = do
+runQuery m mn q@(Query p els) = do
   putStrLn "---"
   case els of
     []     -> return ()
     (e:[]) -> case e of
-      (Literal  l) -> singleL mn l
+      (Literal  l) -> putMT $ singleL mn l
       (Variable v) -> case mn of
-        n@(Just _) -> singleV n v
+        n@(Just _) -> putMT $ singleV n v
         Nothing    -> putStrLn "none"
-    (e:es) -> return ()
+    (e:es) -> putMT $ compound mn els Nothing
   return m
   where
-    singleL :: Maybe Node -> Text -> IO ()
+    compound :: Maybe Node -> [QueryElement] -> Maybe Text -> Maybe Text
+    compound mn ex@(e:es) mt = case e of
+      (Literal  l) -> case mn of
+        Just n  -> case nTerm n == l of
+          True  -> compound (nChild n) es mt
+          False -> compound (nNext  n) ex mt
+        Nothing -> Just "none"
+      (Variable v) -> case mn of
+        Just n  -> compound (nChild n) es $ case mt of
+          Just t  -> Just $ t <> ((v <> ": " <> nTerm n <> "\n"))
+          Nothing -> Just ((v <> ": " <> nTerm n <> "\n"))
+        Nothing -> mt
+    compound Nothing []  mt = mt
+
+    singleL :: Maybe Node -> Text -> Maybe Text
     singleL mn l = case mn of
       Just n  -> case (nTerm n == l) of
-        True  -> putStrLn "true"
+        True  -> Just "true"
         False -> singleL (nNext n) l
-      Nothing -> putStrLn "false"
+      Nothing -> Just "false"
 
-    singleV :: Maybe Node -> Text -> IO ()
-    singleV (Just n) v = do
-      putT (v <> ": " <> nTerm n)
-      singleV (nNext n) v
-    singleV Nothing _  = do return ()
+    singleV :: Maybe Node -> Text -> Maybe Text
+    singleV (Just n) v = Just ((v <> ": " <> nTerm n <> "\n")
+      <> (fromMaybe "" $ singleV (nNext n) v))
+    singleV Nothing _  = Nothing
+
+putMT :: Maybe Text -> IO ()
+putMT (Just t) = putT t
+putMT Nothing  = return ()
 
 putT :: Text -> IO ()
 putT = (putStrLn . T.unpack)
@@ -69,22 +84,8 @@ parse t = case T.splitAt 6 t of
   ("QUERY ", t) -> feQuery t
   (_       ,"") -> Nothing
 
-scrub' :: String -> Text
-scrub' s = pack $ filter syntax s
-  where
-    syntax :: Char -> Bool
-    syntax c = (not . (`elem` ("()," :: [Char]))) c
-
-
-
-isFact :: String -> Bool
-isFact t = "INPUT " `isPrefixOf` t
-
-isQuery :: String -> Bool
-isQuery t = "QUERY " `isPrefixOf` t
-
 scrub :: String -> Text
-scrub s = pack $ filter syntax $ drop 6 s
+scrub s = pack $ filter syntax s
   where
     syntax :: Char -> Bool
     syntax c = (not . (`elem` ("()," :: [Char]))) c
